ee7c488ec348fd18e0c392484e60e8f2a0a69b34
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-07-18 14:18:15 -0300

    FIX: Fix login

--- src/services/_api.js
+++ src/services/_api.js
@@ -4,8 +4,8 @@ import store from '../store/index';
 export default () => axios.create({
   baseURL: process.env.NODE_ENV === 'production'
     ? '#'
-    : 'http://localhost:3001',
+    : 'http://localhost:3000',
   headers: {
-    Authorization: `Bearer ${store.state.auth.token}`,
+    Authorization: `${store.state.auth.token}`,
   },
 });
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,12 +2,12 @@ import Api from './_api';
 
 export default {
   register(credential, recaptcha) {
-    return Api().post('/register', { credential, recaptcha });
+    return Api().post('/users/register', { credential, recaptcha });
   },
   login(credential, recaptcha) {
-    return Api().post('/login', { credential, recaptcha });
+    return Api().post('/users/login', { credential, recaptcha });
   },
   forgotPassword(credential) {
-    return Api().post('/forgot-password', credential);
+    return Api().post('/users/forgot-password', credential);
   },
 };
